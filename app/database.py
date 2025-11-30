cd567626e7a750d72c1f2fba2a2177a6d6919a59
vlad_ustyugov <vladislav.ustyugov@infowatch.com>
2025-12-01 02:49:26 +0700

    feat: alembic migrations

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
 # Строка подключения для SQLite
-DATABASE_URL = "sqlite:///ecommerce.db"
+DATABASE_URL = "sqlite:///cybermarket.db"
 
 # Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
